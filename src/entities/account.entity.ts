b06d08b1f76cf1fafc6d929cd7a38809ec5134e8
Ian Bautista <victor.jackal@gmail.com>
2021-06-26 23:00:13 +0800

    WIP: account/swagger implementation

--- src/entities/account.entity.ts
+++ src/entities/account.entity.ts
@@ -1,16 +1,23 @@
+import { ApiProperty } from "@nestjs/swagger";
 import shortUuid from "short-uuid";
 import { BeforeInsert, Column, Entity, PrimaryColumn } from "typeorm";
 
 @Entity()
 export class Account {
   @PrimaryColumn()
+  @ApiProperty()
   id: string;
 
   @Column({ nullable: false, unique: true })
+  @ApiProperty()
   email: string;
 
   @Column()
   name: string;
+
+  @Column({ type: "datetime", default: () => "CURRENT_TIMESTAMP" })
+  @ApiProperty()
+  added: Date;
 
   @BeforeInsert()
   setId() {
